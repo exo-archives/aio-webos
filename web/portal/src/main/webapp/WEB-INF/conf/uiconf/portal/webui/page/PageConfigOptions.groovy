cffba71a3ebafb7abc43a16e2771aafaa0cdda09
Nguyen Ba Phu <phu.nguyen@exoplatform.com>
2008-04-08 11:08:33 +0000

    create webos branches 1.0

---
+++ web/portal/src/main/webapp/WEB-INF/conf/uiconf/portal/webui/page/PageConfigOptions.groovy
@@ -0,0 +1,17 @@
+import org.exoplatform.webui.core.model.SelectItemOption;
+import org.exoplatform.webui.core.model.SelectItemCategory;
+
+List categories = new ArrayList(); 
+String config = null ;
+
+SelectItemCategory normalPageConfigs = new SelectItemCategory("normalPageConfigs") ;
+categories.add(normalPageConfigs);
+config = "<page>" +
+				 "	<owner-type></owner-type>" +
+  			 "	<owner-id></owner-id>" +
+				 "	<name></name>" +
+				 "	<factory-id>Desktop</factory-id>" +
+				 "</page>" ;
+normalPageConfigs.addSelectItemOption(new SelectItemOption("Desktop Layout", config, "DesktopImage"));
+
+return categories;
