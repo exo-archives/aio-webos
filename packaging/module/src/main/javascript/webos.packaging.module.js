797c19a93edddf85c64c12f3e606f95c897fa488
Dimitri Baeli <dimitri.baeli@exoplatform.com>
2009-12-14 04:39:22 +0000

    WEBOS-306: Build - Integrate module.js for kernel in the project to be used by exopackage and maven-exobuild-plugin

---
+++ packaging/module/src/main/javascript/webos.packaging.module.js
@@ -0,0 +1,27 @@
+eXo.require("eXo.projects.Module") ;
+eXo.require("eXo.projects.Product") ;
+
+function getModule(params) {
+
+  var kernel = params.kernel;
+  var core = params.core;
+  var eXoPortletContainer = params.eXoPortletContainer;
+  var jcr = params.eXoJcr;
+
+  var module = new Module();
+
+  module.version = "${project.version}" ;
+  module.relativeMavenRepo =  "org/exoplatform/webos" ;
+  module.relativeSRCRepo =  "webos" ;
+  module.name =  "webos" ;
+                   
+  module.web = {}
+  module.web.webosResources = 
+  	new Project("org.exoplatform.webos", "exo.webos.web.webosResources", "war", module.version) ;
+  module.web.webosportal = 
+    new Project("org.exoplatform.webos", "exo.webos.web.portal", "exo-portal", module.version).
+    addDependency(jcr.frameworks.web).
+    addDependency(jcr.frameworks.command) ;
+
+  return module;
+}
