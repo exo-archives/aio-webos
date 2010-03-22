a45c282bdf261ff9b7d1cf999564c9f75fa5126e
Tan Pham Dinh <tan.pham@exoplatform.com>
2010-03-22 04:07:09 +0000

    Prepare POM for deploying with GateIn

--- packaging/product/src/main/javascript/webos.packaging.product.js
+++ packaging/product/src/main/javascript/webos.packaging.product.js
@@ -14,10 +14,9 @@ function getProduct(version) {
   var kernel = Module.GetModule("kernel") ;
   var core = Module.GetModule("core") ;
   var ws = Module.GetModule("ws");
-  var eXoPortletContainer = Module.GetModule("portletcontainer", {kernel : kernel, core : core}) ;    
   var eXoJcr = Module.GetModule("jcr", {kernel : kernel, core : core, ws : ws}) ;
-  var portal = Module.GetModule("portal", {kernel : kernel, ws:ws, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr});
-  var webos = Module.GetModule("webos", {kernel : kernel, core : core, eXoPortletContainer : eXoPortletContainer, eXoJcr : eXoJcr });
+  var portal = Module.GetModule("portal", {kernel : kernel, ws:ws, core : core,eXoJcr : eXoJcr});
+  var webos = Module.GetModule("webos", {kernel : kernel, core : core, eXoJcr : eXoJcr });
   
   product.addDependencies(portal.web.rest) ;
   product.addDependencies(portal.portlet.exoadmin) ;
@@ -28,20 +27,25 @@ function getProduct(version) {
   product.addDependencies(portal.webui.portal);
   
 	product.addDependencies(portal.web.eXoResources);
-	product.addDependencies(portal.web.eXoMacSkin);
-	product.addDependencies(portal.web.eXoVistaSkin);
 	product.addDependencies(webos.web.webosResources);
+	product.addDependencies(portal.web.portal) ;
+  product.addDependencies(portal.sample.extension) ;
+
+  product.addDependencies(portal.ibm.jdk.support);
 	
   product.addDependencies(webos.web.webosportal) ;
   
   product.addServerPatch("tomcat", portal.server.tomcat.patch) ;
   product.addServerPatch("jboss",  portal.server.jboss.patch) ;
   product.addServerPatch("jbossear",  portal.server.jbossear.patch) ;
-  product.addServerPatch("jonas",  portal.server.jonas.patch) ;
-  product.addServerPatch("wasear",  portal.server.websphere.patch) ;
+  
+  /* cleanup duplicated lib */
+  product.removeDependency(new Project("commons-httpclient", "commons-httpclient", "jar", "3.0"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.1"));
+  product.removeDependency(new Project("commons-collections", "commons-collections", "jar", "3.2"));
   
   product.module = webos ;
-  product.dependencyModule = [kernel, core, eXoPortletContainer, ws, eXoJcr, portal];
+  product.dependencyModule = [kernel, core, ws, eXoJcr, portal];
     
   return product ;
 }
